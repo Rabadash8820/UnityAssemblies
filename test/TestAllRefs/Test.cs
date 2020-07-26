d189c7453b3489cef103260383988c6dc1176314
Dan Vicarel <shundra8820@gmail.com>
2020-07-26 12:31:03 -0400

    Improved how TestAllRefs references assemblies from different Unity versions
    - Test.cs now has #define directives for whether a given library is available for a given Unity version
    - Now references all assemblies for which we provide short-hand properties

--- test/TestAllRefs/Test.cs
+++ test/TestAllRefs/Test.cs
@@ -1,13 +1,43 @@
-﻿namespace Unity3D.Test {
+#if v20192
+#define NUNIT_AVAILABLE
+#endif
+#if v20193
+#define NUNIT_AVAILABLE
+#endif
+
+#if v20192
+#define MOQ_AVAILABLE
+#endif
+#if v20193
+#define MOQ_AVAILABLE
+#endif
+
+#if v20192
+#define UNITY_ANALYTICS_STANDARD_EVENTS_AVAILABLE
+#endif
+#if v20193
+#define UNITY_ANALYTICS_STANDARD_EVENTS_AVAILABLE
+#endif
+
+
+namespace Unity3D.Test {
     public class TestBehaviour : UnityEngine.MonoBehaviour {
         public UnityEngine.UI.Text TextField;
         public UnityEngine.TestTools.TestPlatform TestPlatformField;
+#if UNITY_ANALYTICS_STANDARD_EVENTS_AVAILABLE
+        public UnityEngine.Analytics.ContinuousEvent ContinuousEventField;
+#endif
+
     }
 
     public class TestEditor : UnityEditor.Editor { }
 
-#if v20192
+#if NUNIT_AVAILABLE
     [NUnit.Framework.TestFixture]
 #endif
-    public class TestFixture { }
+    public class TestFixture {
+#if MOQ_AVAILABLE
+        public Moq.Mock<TestBehaviour> Mock;
+#endif
+    }
 }
